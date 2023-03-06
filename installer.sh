b591033b43820c40d371a44876798c7e293593ee
Duckteam <69460253+RealDuckteam@users.noreply.github.com>
2023-03-06 19:13:54 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+    echo -e "${CYAN}Bitte führen sie dieses Skript als Root aus!"
+    exit
+fi
+
+clear
+
+
+block(){
+# Update the package list
+sudo apt update
+
+# Install Apache2 package
+sudo apt install apache2 -y
+
+# Enable Apache2 on system startup
+sudo systemctl enable apache2
+
+# Start Apache2 service
+sudo systemctl start apache2
+
+# Check Apache2 status
+sudo systemctl status apache2
+}
+
+block1(){
+    while true; do
+        read -p "Sind Sie sicher, dass Sie Apache2 auf ihrem Gerät/Server installieren möchten? [y/n]? " yn
+        case $yn in
+            [Yy]* ) block; break;;
+            [Nn]* ) exit;;
+            * ) echo "Bitte antworten mit Ja [y] oder Nein [n]";;
+        esac
+    done
+}
